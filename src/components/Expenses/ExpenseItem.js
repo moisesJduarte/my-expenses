2206cfd6e84a70356d106cd0dc7b6790a535cc38
moisesJduarte <gladmd19@gmail.com>
2023-01-25 16:48:47 -0500

    adding filter update

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
-import Card from '../UI/Card';
 
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
-
+    // function clickHandler() {}
     const [title, setTitle] = useState(props.title);
+    console.log('ExpenseItem evaluated by React');
 
     const clickHandler = () => {
-        setTitle('Update');
-
+        setTitle('Updated!');
         console.log(title);
-    }
+    };
 
     return (
         <Card className='expense-item'>
@@ -22,7 +21,7 @@ const ExpenseItem = (props) => {
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change title</button>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,6 +10,12 @@ const Expense = (props) => {
     const filterChangeHandler = selectedYear => {
         setFilterYear(selectedYear);
     };
+
+    const filteredExpense = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+
     return (
         <div>
             <Card className='expenses'>
@@ -17,13 +23,17 @@ const Expense = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {props.items.map((expense) => (
-                    <ExpenseItem
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
+                {filteredExpense.length === 0 && <p>No expenses found.</p>}
+                {filteredExpense.length === 0 &&
+                    filteredExpense.map((expense) => (
+                        <ExpenseItem
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />
+                    ))}
+
             </Card>
         </div>
     );
